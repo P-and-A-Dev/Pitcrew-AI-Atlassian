eca51cb7c7cabf2d05b222b5e9225418318aef9a
Pietro Giacomelli <pietrogiacomelli8@gmail.com>
2025-12-13 01:30:07 +0000

    header.tst and dashboardLayout.tsx and uptade for diferents screens

--- apps/dashboard/src/layout/DashboardLayout.tsx
+++ apps/dashboard/src/layout/DashboardLayout.tsx
@@ -4,7 +4,7 @@ interface Props {
     children: React.ReactNode
 }
 
-export default function DashboardLayout({children}: Props) {
+export default function DashboardLayout({ children }: Props) {
     return (
         <div className="min-h-screen bg-[#0A0F1F] text-white">
             <Header/>
